1e874032afe900a3f68bab7d9f8fa60e400e0259
moonsonu <moonk90@gmail.com>
2018-08-31 17:58:13 -0700

    v2 c

--- sommelier.py
+++ sommelier.py
@@ -2,14 +2,19 @@
 # coding: utf-8
 
 # # V.1 Exploring the green reds
-# ## a) function that plot scatterplot matrix of red wine data
+# ### a) function that plot scatterplot matrix of red wine data
 
-# In[4]:
+# In[79]:
 
 
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import random
+
+
+# In[119]:
+
 
 def plot_scatter_matrix(wine_data, good_threshold, bad_threshold, save_plot):
     df = pd.read_csv(wine_data, sep=';')
@@ -36,38 +41,57 @@ plot_scatter_matrix('winequality-red.csv', 8, 3, save_plot=False)
 # 
 
 # # V.2 Learning to perceptron
-# ## a) & b) implement a perceptron
+# ### a) & b) implement a perceptron
 
-# In[70]:
+# In[193]:
 
-
-import pandas as pd
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import random
 
 class perceptron():
-    def __init__(self, thresholds=0.0, eta=0.01, n_iter=10):
+    def __init__(self, thresholds=0.0, eta=0.01, n_iter=14000):
         self.thresholds = thresholds
         self.eta = eta
         self.n_iter = n_iter
     def fit(self, X, y):
         self.w_ = []
+        result_ = []
+        num_epoch = 0
         for i in range(1 + X.shape[1]):
             self.w_.append(random.random())
-        print(self.w_)
         self.errors_ = []
-
-        for _ in range(self.n_iter):
-            errors = 0
-            for xi, target in zip(X, y):
-                update = self.eta * (int(target) - self.predict(xi))
-                self.w_[1:] += update * xi
-                self.w_[0] += update
-                errors += int(update != 0.0)
-            self.errors_.append(errors)
-            #print(self.w_)
-        return self
+        if (self.n_iter != 0):
+            for _ in range(self.n_iter):
+                errors = 0
+                result = 0
+                num_epoch += 1
+                for xi, target in zip(X, y):
+                    update = self.eta * (int(target) - self.predict(xi))
+                    self.w_[1:] += update * xi
+                    self.w_[0] += update
+                    errors += int(update != 0.0)
+                    result = (num_epoch, errors, self.w_[1:], self.w_[0])
+                self.errors_.append(errors)
+                result_.append(result)
+                
+        else:
+            while (1):
+                errors = 0
+                result = 0
+                num_poch += 1
+                for xi, target in zip(X, y):
+                    update = self.eta * (int(target) - self.predict(xi))
+                    self.w_[1:] += update * xi
+                    self.w_[0] += update
+                    errors += int(update != 0.0)
+                    result = (num_epoch, errors, self.w_[1:], self.w_[0])
+                self.errors_.append(errors)
+                result_.append(result)
+                if (errors == 0):
+                    break
+                
+                
+                #print(self.w_)
+        
+        return result_
     def net_input(self, X):
         activation = self.w_[0]
         for i in range(len(X)):
@@ -78,18 +102,59 @@ class perceptron():
             return 1
         else:
             return -1
-df = pd.read_csv('winequality-red.csv', sep=';')
-df_filtered = df[(df.quality >= 8) | (df.quality <= 3)]
-df_data = df_filtered[['alcohol', 'pH', 'quality']]
-y = ['1' if k >= 8 else '-1' for k in df_data['quality']]
-X = df_data[['alcohol', 'pH']].values
-p = perceptron(eta=0.1)
-p.fit(X, y)
-print(p.errors_)
+            
+p = perceptron(eta=0.01)
 
 
-# In[ ]:
+# ### c) function that will take the output of perceptron training function and generate two plots in one figure
+
+# In[217]:
 
 
+def plot_performance(performance, wine_data, good_thresh, bad_thresh, epoch=-1, save_plot=False):
+    df = pd.read_csv(wine_data, sep=';')
+    df_filtered = df[(df.quality >= good_thresh) | (df.quality <= bad_thresh)]
+    df_data = df_filtered[['alcohol', 'pH', 'quality']]
+    y = ['1' if k >= 8 else '-1' for k in df_data['quality']]
+    X = df_data[['alcohol', 'pH']].values
+    data = p.fit(X, y)
 
+    numvar = X.shape[0]
+    x1_ = []
+    y1_ = []
+    x2_ = []
+    y2_ = [] 
+    for i in data:
+        x1 = i[0]
+        y1 = i[1]
+        
+        x1_.append(x1)
+        y1_.append(y1)
+        if i[1] == 0 :
+            for j in range(numvar):
+                x2 = X[j]
+                slop = -i[2][0] / i[2][1]
+                y2 = slop * x2
+                x2_.append(x2)
+                y2_.append(y2)
+            break
+
+    print(x2_)
+    print(y2_)
+    plt.subplot(121)
+    plt.plot(x1_, y1_, color='red')
+    plt.xlabel('epoch')
+    plt.ylabel('errors')
+    plt.title("Errors as a function of epoch")
+
+    plt.subplot(122)
+    color = ['blue' if k >= good_thresh else 'red' for k in df_data['quality']]
+    size = [10 if k >= good_thresh else 10 if k <= bad_thresh else 0 for k in df_data['quality']]
+    plt.scatter(df_data['alcohol'], df_data['pH'], c = color, s = size)
+    plt.plot(x2_, y2_)
+    plt.xlabel('alcohol')
+    plt.ylabel('pH')
+    plt.title("Decision boundary on epoch:")
+    plt.show()
+plot_performance(p, 'winequality-red.csv', 8, 3, epoch = 100, save_plot=False)
 
